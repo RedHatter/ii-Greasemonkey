0fece20832c2231e4fd7d9be79c45925e925ef8a
Ava Johnson <ava.johnson@zohomail.com>
2022-06-24 14:56:47 -0700

    [ii-junk] Remove dependency on jquery

--- ii-junk.user.js
+++ ii-junk.user.js
@@ -2,7 +2,6 @@
 // @name        ii-junk
 // @description Lists item price, price per kg, and total bag value in inventory.
 // @namespace   http://idioticdev.com
-// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // @include     http://*improbableisland.com/inventory.php*
 // @include     https://*improbableisland.com/inventory.php*
 // @include     http://*improbableisland.com/badnav.php
@@ -10,45 +9,49 @@
 // @include     http://*improbableisland.com/runmodule.php?module=eboy*
 // @include     https://*improbableisland.com/runmodule.php?module=eboy*
 // @version     1
-// @grant       GM_setValue
-// @grant       GM_getValue
 // ==/UserScript==
-let prices = JSON.parse(GM_getValue('prices', '{}'))
+const priceValues = JSON.parse(localStorage.getItem('priceValues')) ?? {}
 
 if (window.location.href.includes('inventory')) {
-  let backpack = processBag($('b:contains("Backpack")').closest('table').next())
-  $('b:contains("Backpack")').after(` &mdash; ${backpack} req`)
-  let bandolier = processBag($('b:contains("Bandolier")').closest('table').next())
-  $('b:contains("Bandolier")').after(` &mdash; ${bandolier} req`)
+  const backpackHeading = document.evaluate("//b[contains(., 'Backpack')]", document).iterateNext()
+  const backpackTable = document.evaluate("ancestor::table", backpackHeading).iterateNext()
+  const backpackValue = caluclatePricesForElement(backpackTable)
+  backpackHeading.insertAdjacentHTML('afterend', ` &mdash; ${backpackValue} req`)
+
+  const bandolierHeading = document.evaluate("//b[contains(., 'Bandolier')]", document).iterateNext()
+  const bandolierTable = document.evaluate("ancestor::table", bandolierHeading).iterateNext()
+  const bandolierValue = caluclatePricesForElement(bandolierTable)
+  bandolierHeading.insertAdjacentHTML('afterend', ` &mdash; ${bandolierValue} req`)
 } else {
-  $('span:contains("Buying at")').each((i, e) => {
-    let name = $(e).closest('table').parent().children().eq(0).text()
-    let price = $(e).text()
-    price = parseInt(price.substring(price.indexOf(':') + 2))
-    prices[name] = price
-  })
-  GM_setValue('prices', JSON.stringify(prices))
+  const itemIterator = document.evaluate("//span[contains(., 'Buying at')]", document)
+  let itemPriceElement
+
+  while (itemPriceElement = itemIterator.iterateNext()) {
+    const itemName = document.evaluate("ancestor::table[2]//b", itemPriceElement).iterateNext().textContent
+    const priceText = itemPriceElement.textContent
+    const price = parseInt(priceText.substring(priceText.indexOf(': ') + 2))
+    priceValues[itemName] = price
+  }
+
+  localStorage.setItem('priceValues', JSON.stringify(priceValues))
 }
 
-function processBag (bag) {
-  let total = 0
-  for (let item in prices) {
-    let name = $(`b:contains("${item}")`, bag)
-    if (name.length < 1) continue
-    
-    let value = prices[item]
-    
-    let text = $(name).parent().text()
-    let weight = parseFloat(text.substring(text.indexOf('Weight: ') + 8, text.indexOf(' kg')))
-    let rqk = value / weight
-    
-    if (text.includes('Quantity'))
-      value *= parseInt(name.next().next('b').text())
+function caluclatePricesForElement (element) {
+  return Object.entries(priceValues).reduce((total, [itemName, itemValue]) => {
+    const itemNameElement = document.evaluate(`.//b[text()='${itemName}']`, element).iterateNext()
+    if (itemNameElement === null) return total
+        
+    const text = itemNameElement.parentNode.textContent
+    const { groups: { quantity, weight } } = text.match(/(?:Quantity: (?<quantity>[\d,]+) \| )?Weight: (?<weight>[\d,.]+) kg/)
 
-    name.next().after(`Value: <b>${value}</b> req, ${Math.round(rqk * 100) / 100} per kg | `)
+    const rqk = Math.round((itemValue / parseFloat(weight)) * 100) / 100
+    
+    if (quantity !== undefined) {
+      itemValue *= parseInt(quantity)
+    }
+
+    itemNameElement.nextElementSibling.insertAdjacentHTML('afterend', `Value: <b>${itemValue}</b> req, ${rqk} per kg | `)
       
-    total += value
-  }
-  
-  return total
+    return total + itemValue
+  }, 0)
 }
